211ff10fca7b8579fb71002a8d322758a4277aaf
Maksim002 <inoplanee@Mail.ru>
2020-09-26 13:40:06 +0600

    26.09.2020

--- app/src/main/java/com/example/android/navigationadvancedsample/homescreen/About.kt
+++ app/src/main/java/com/example/android/navigationadvancedsample/homescreen/About.kt
@@ -21,6 +21,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -43,18 +44,12 @@ class About : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         iniRecyclerView(view)
-
-
-//        viewModel.error.observe(viewLifecycleOwner, Observer {
-//            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
-//        })
     }
 
     init {
         val map = HashMap<String, String>()
         map.put("login", "143382")
         map.put("token", "d67d8ab4f4c10bf22aa353e27879133c")
-
         if (viewModel.listNewDta.value == null)
             viewModel.listNews(map)
     }
@@ -62,7 +57,6 @@ class About : Fragment() {
     private fun iniRecyclerView(view: View) {
         viewModel.listNewDta.observe(viewLifecycleOwner, Observer { result ->
             view.findViewById<TextView>(R.id.data_text).text = result.result[position].description
-
         })
     }
 }
